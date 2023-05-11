944dc66f062fd5b809f22a7ac49481329a0b6a93
leimartin <leimartin1603@gmail.com>
2023-05-12 03:37:16 +0800

    added some comments

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+// Main Activity (230511)
+// MARTIN, Katryna Lei V.
+// 2nd Year BSCS - AN22
+
+// 3 global variables w/o init
 let usename;
 let password;
 let role;
 
+// console log the message "Hello World" to ensure that the script file is properly associated with the html file
 console.log('Hello World!')
 
 function login() {
+	// user input section
 	username = prompt('Enter username: ');
 	password = prompt('Enter password: ');
 	role = prompt('Enter role: ');
 
+	/* check 
+		if the username is an empty string or null or 
+		if the password is an empty string or null or 
+		if the role is an empty string or null */
+
 	if(username==="" || username===null || 
-		password==="" || password===null ||
+		password==="" || password===null||
 		role==="" || role===null) {
+		// alert to inform the user that their input should not be empty
 		alert("Warning!\n\nUsername/Password/Role should not be empty!");
 	} else {
 		switch(role.toLowerCase()) {
@@ -31,26 +44,30 @@ function login() {
 		
 }
 
+// calling the login function
 login();
+// inserting a space to make it readable
 console.log();
 
+// function that takes 4 arguments
 function average(n1, n2, n3, n4) {
+	// calculate its average
 	avg = Math.round((n1 + n2 + n3 + n4) / 4);
+
+	// checking if the Math.round is working
 	console.log("Average: " + avg)
 
+	// log a message accdg to the calculated average
 	if(avg <= 74) {
-		// show average here
-		alert("Hello, student, your average is " + avg + ". The letter equivalent is F.")
+		console.log("Hello, student, your average is " + avg + ". The letter equivalent is F.")
 	} else if(avg >= 85 && avg <= 89) {
-		// show average here
-		alert("Hello, student, your average is " + avg + ". The letter equivalent is B.")
+		console.log("Hello, student, your average is " + avg + ". The letter equivalent is B.")
 	} else if(avg >= 90 && avg <= 95) {
-		// show average here
-		alert("Hello, student, your average is " + avg + ". The letter equivalent is A.")
+		console.log("Hello, student, your average is " + avg + ". The letter equivalent is A.")
 	} else if (avg > 96) {
-		// show average here
-		alert("Hello, student, your average is " + avg + ". The letter equivalent is A+.")
+		console.log("Hello, student, your average is " + avg + ". The letter equivalent is A+.")
 	}
 }
 
-average(86, 92, 89, 96);
+// calling the average function along with its 4 numbers as arguments
+average(86, 92, 75, 96)
